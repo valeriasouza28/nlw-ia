90f0e7a768746ea9508cc2ba1b8cd7406d86bcea
valeriasouza28 <valleriasouza28@gmail.com>
2023-10-09 00:06:13 +0000

    up

--- upload-ai-api/src/routes/create-transcription.ts
+++ upload-ai-api/src/routes/create-transcription.ts
@@ -1,23 +1,22 @@
 import { FastifyInstance } from "fastify";
-import {z} from "zod";
-import {prisma} from "../lib/prisma";
-import { uploadVideoRoute } from "./upload-video";
 import { createReadStream } from "node:fs";
-import { openai } from "../lib/opena-ai";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
-  app.post('/videos/:videoId/transcription', async(req) => {
+  app.post('/videos/:videoId/transcription', async (req) => {
     const paramsSchema = z.object({
       videoId: z.string().uuid(),
-    });
+    })
 
-    const {videoId} = paramsSchema.parse(req.params)
-    
+    const { videoId } = paramsSchema.parse(req.params)
+
     const bodySchema = z.object({
       prompt: z.string(),
-    });
+    })
 
-    const {prompt} = bodySchema.parse(req.body);
+    const { prompt } = bodySchema.parse(req.body)
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
@@ -26,20 +25,30 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
     })
 
     const videoPath = video.path
-
     const audioReadStream = createReadStream(videoPath)
 
     const response = await openai.audio.transcriptions.create({
-        file: audioReadStream,
-        model: "whisper-1",
-        language: "pt",
-        response_format: "json",
-        temperature: 0,
-        prompt
+      file: audioReadStream,
+      model: 'whisper-1',
+      language: 'pt',
+      response_format: 'json',
+      temperature: 0,
+      prompt,
     })
 
-    return response.text
+    const transcription = response.text
 
-  });
+    await prisma.video.update({
+      where: {
+        id: videoId,
+      },
+      data: {
+        transcription,
+      }
+    })
 
+    return {
+      transcription,
+    }
+  })
 }
